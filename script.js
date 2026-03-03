4fe00a090881b8dd5376ed0e38b3622d8bee9b3f
BojanapuManikanth <manikanthbojanapu@thestackly>
2026-03-03 17:38:58 +0530

    Files committing

---
+++ script.js
@@ -0,0 +1,7 @@
+let name1 = "Manikanth";
+let age = 23;
+let city = "Bangalore";
+
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I am from " + city + ".");
