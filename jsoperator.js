9562b256cc7a7f8e91e21b37e2008b63042148a4
BojanapuManikanth <manikanthbojanapu@thestackly>
2026-03-04 18:22:19 +0530

    first commit

---
+++ jsoperator.js
@@ -0,0 +1,180 @@
+console.log("---Section 1---");
+console.log(25+4);
+console.log(25-4);
+console.log(25*4);
+console.log(25/4);
+console.log(100%30);
+console.log(7**2);
+console.log(2**5);
+let num =50;
+console.log(num/5);
+console.log(45 % 6);
+console.log(0.5 * 8);
+console.log(75 - 200);
+console.log(10 + 20 + 30);
+console.log(9 % 2 );
+
+console.log("---Section 2---");
+let a = 5;
+console.log(a++);
+console.log(a);
+    a = 5;  
+console.log(++a);
+console.log(a);
+    a = 10;
+let b = a++;
+console.log(a, b);
+    a = 10;
+    b = ++a;
+console.log(a, b);
+   a = 1;
+   b = a++ + a++;
+console.log(a, b);
+   a = 1;
+   b = ++a + ++a;
+console.log(a, b);
+   a = 3;
+   a = a++;
+console.log(a);
+   a = 3;
+   a = ++a;
+console.log(a);
+let x = 5;
+let y = --x;
+console.log(x, y);
+   x = 5;
+   y = x--;
+console.log(x, y);
+    a = 2;
+    b = a++ + ++a + a++;
+console.log(a, b);
+    a = 4;
+    b = --a + a-- + --a;
+console.log(a, b);
+    a = 1;
+    b = ++a + a++ + ++a;
+console.log(a, b);
+    a = 5;
+    b = a++ + a++ + a++;
+console.log(a, b);
+    a = 10;
+    a += a++;
+console.log(a);
+
+console.log("---Section 3---");
+let n1 = 10;
+n1 += 5;
+console.log(n1);
+let n2 = 20;
+n2 -= 8;
+console.log(n2);
+let n3 = 7;
+n3 *= 3;
+console.log(n3);
+let n4 = 40;
+n4 /= 5;
+console.log(n4);
+let n5 = 25;
+n5 %= 4;
+console.log(n5);
+
+a = 5;
+a += 3;
+a *=2;
+console.log(a);
+a = 10;
+a -= 2;
+a /=4;
+console.log(a);
+a = 8;
+a %= 3;
+console.log(a);
+a = 6;
+a += a;
+console.log(a);
+a = 2;
+a *= a += 3;
+console.log(a);
+
+console.log("---Section 4---");
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(0 == false);
+console.log(0 === false);
+console.log(null == undefined);
+console.log(null === undefined);
+console.log("10" > 5);
+console.log("2" > "10");
+console.log(true == 1);
+console.log(true === 1);
+
+console.log("---Section 5---");
+console.log(true && false);
+console.log(true || false);
+console.log(false || false);
+console.log(!true);
+console.log(!(5 > 2));
+console.log(5 > 3 && 10 > 5);
+console.log(5 > 3 && 10 < 5);
+console.log(5 < 3 || 10 < 5);
+console.log(5 < 3 || 10 > 5);
+console.log((5 > 2 && 3 > 1) || (10 < 2));
+
+console.log("---Section 6---");
+let age = 20;
+console.log( age >= 18 ? "Adult" : "Minor");
+let marks = 60;
+console.log(marks >= 50 ? "Pass" : "Fail");
+let num1 = 9;
+console.log(num1 % 2 == 0 ? "Even" : "Odd");
+let score = 85;
+console.log(score >= 90 ? "Grade A" : score >= 80 ? "Grade B" : score >= 70 ? "Grade C" : "Grade F");
+let value = -5;
+console.log(value > 0 ? "Positive" : value < 0 ? "Negative" : "Zero");
+
+console.log("---Section 7---");
+console.log("Hello " + "World");
+let firstName = "Mani";
+let lastName = "Kanth";
+console.log(firstName +" "+lastName);
+console.log(`${firstName} ${lastName}`);
+console.log("10" + 5);
+console.log(5 + "10");
+
+console.log("---Section 8---");
+console.log("5" + 2);
+console.log("5" - 2);
+console.log("5" * 2);
+console.log(true + 1);
+console.log(false + "1");
+
+console.log("---Section 9---");
+console.log(Number("123"));
+console.log(Number("abc"));
+console.log(Number(true));
+console.log(Boolean(""));
+console.log(Boolean("Hello"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ 
+ 
+ 
+    
+
+   
+
